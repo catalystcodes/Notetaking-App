35ad3ab64dc8c4ac32389ffe23f9ff13fa77ae59
catalystcodes <muh4mub@gmail.com>
2024-02-27 20:56:33 +0100

    feat: onboarding1

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Onboarding1 from "./src/component/onboarding1";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Onboarding1 />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  container: {},
 });
---
+++ src/component/onboarding1.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+
+import {
+  View,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  Pressable,
+} from "react-native";
+
+const Onboarding1 = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text
+          style={{
+            textAlign: "right",
+            fontSize: 14,
+            fontWeight: "500",
+            marginTop: 35,
+            marginRight: 26,
+          }}
+        >
+          SKIP
+        </Text>
+        <Image
+          style={styles.image1}
+          source={require("../../assets/images/conifer-writer.png")}
+        />
+      </View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <Image source={require("../../assets/images/Ellipse1.png")} />
+        <Image
+          style={{ marginLeft: 8 }}
+          source={require("../../assets/images/Ellipse2.png")}
+        />
+      </View>
+      <Text
+        style={{
+          textAlign: "center",
+          marginTop: 35,
+          fontWeight: 700,
+          fontSize: 20,
+        }}
+      >
+        An Actual notebook in
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          fontWeight: 700,
+          fontSize: 20,
+        }}
+      >
+        your pocket
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          marginLeft: 33,
+          marginRight: 34,
+          marginTop: 15,
+          lineHeight: 23.87,
+        }}
+      >
+        Take notes quickly and easily. Get your ideas
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          marginLeft: 33,
+          marginRight: 34,
+        }}
+      >
+        {" "}
+        penned down,
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          marginLeft: 33,
+          marginRight: 34,
+        }}
+      >
+        where ever and when ever
+      </Text>
+      <Pressable
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 100,
+          marginTop: 45,
+          paddingTop: 20,
+          backgroundColor: "#7D57F1",
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+          }}
+        >
+          Get Started
+        </Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  image1: {
+    marginTop: 23,
+    marginLeft: 45,
+    marginBottom: 86,
+    width: 343,
+  },
+});
+
+export default Onboarding1;
