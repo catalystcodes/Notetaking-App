6b7de4d531e230316735d601223ae9dd30f4e6ad
catalystcodes <muh4mub@gmail.com>
2024-03-02 16:01:24 +0100

    feat: update database

---
+++ src/component/icons/changePasswordIcon.jsx
@@ -0,0 +1,4 @@
+<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="18" cy="18" r="18" fill="#7D57F1"/>
+<path d="M22 14H14M22 14C24.2091 14 26 15.7909 26 18V24C26 26.2091 24.2091 28 22 28H14C11.7909 28 10 26.2091 10 24V18C10 15.7909 11.7909 14 14 14M22 14V12C22 9.79086 20.2091 8 18 8C15.7909 8 14 9.79086 14 12V14M18 22V20" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
+</svg>
---
+++ src/component/icons/personalDetails.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Svg, { Circle, Path } from "react-native-svg";
+const personalDetails = (props) => (
+  <Svg width={24} height={24} fill="none" {...props}>
+    <Circle cx={18} cy={18} r={18} fill="#7D57F1" />
+    <Path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M28 18a9.976 9.976 0 0 1-3.441 7.549A9.961 9.961 0 0 1 18 28a9.961 9.961 0 0 1-6.559-2.451A9.977 9.977 0 0 1 8 18c0-5.523 4.477-10 10-10s10 4.477 10 10Zm-10 7a6.973 6.973 0 0 0 5-2.101C21.92 21.15 20.083 20 18 20s-3.92 1.15-5 2.899A6.974 6.974 0 0 0 18 25Zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+      clipRule="evenodd"
+    />
+  </Svg>
+);
+export default personalDetails;
