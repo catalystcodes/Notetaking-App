168eefaa9fa9647fd66bc1f3f4966089c95c8d64
catalystcodes <muh4mub@gmail.com>
2024-03-30 13:52:51 +0100

    update database

---
+++ src/component/card.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const Card = () => {
+  return (
+    <View style={styles.view1}>
+      <View style={styles.view2}></View>
+      <View style={styles.view3}>
+        <Text style={styles.text1}>Randoms</Text>
+        <Text>
+          This is some random thoughts, I’m still thinking of what to make of
+          this. Hmm! what should I focus on or....
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  view1: {
+    marginLeft: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  view2: {
+    backgroundColor: "#FDC1BA",
+    width: 10,
+    height: 98,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  view3: {
+    backgroundColor: "#fff",
+    height: 98,
+    width: 380,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  text1: {
+    fontWeight: "600",
+    fontSize: 16,
+    marginBottom: 10,
+    marginTop: 15,
+  },
+});
+
+export default Card;
--- src/screens/mainHome.jsx
+++ src/screens/mainHome.jsx
@@ -6,8 +6,10 @@ import {
   Text,
   TextInput,
   SafeAreaView,
+  ScrollView,
 } from "react-native";
 import SearchArea from "../component/searchArea";
+import Card from "../component/card";
 
 const MainHome = () => {
   return (
@@ -28,14 +30,14 @@ const MainHome = () => {
             <View>
               <View style={styles.view3}>
                 <Image source={require("../../assets/images/myNote.png")} />
-                <Text>My Notes</Text>
+                <Text style={styles.text2}>My Notes</Text>
               </View>
             </View>
 
             <View>
               <View style={styles.view3}>
                 <Image source={require("../../assets/images/favorites.png")} />
-                <Text>Favorites</Text>
+                <Text style={styles.text2}>Favorites</Text>
               </View>
             </View>
           </View>
@@ -44,19 +46,44 @@ const MainHome = () => {
             <View>
               <View style={styles.view3}>
                 <Image source={require("../../assets/images/Categories.png")} />
-                <Text>Categories</Text>
+                <Text style={styles.text2}>Categories</Text>
               </View>
             </View>
             <View>
               <View style={styles.view3}>
                 <Image source={require("../../assets/images/shared.png")} />
-                <Text>Shared</Text>
+                <Text style={styles.text2}>Shared</Text>
               </View>
             </View>
           </View>
         </View>
 
         <Text style={styles.text1}>Recents</Text>
+        <View style={styles.view5}>
+          <View style={styles.view6}>
+            <View style={styles.view8}>
+              <Text style={styles.text3}>Design Thinking</Text>
+              <Text style={styles.text4}>20/05/2022</Text>
+            </View>
+            <Text style={styles.text5}>Design thinking is the</Text>
+            <Text style={styles.text5}>strategic and practical proce...</Text>
+          </View>
+          <View style={styles.view7}>
+            <View style={styles.view8}>
+              <Text style={styles.text3}>Zuriboard Training</Text>
+              <Text style={styles.text4}>20/05/2022</Text>
+            </View>
+            <Text style={styles.text5}>Introduction to product design</Text>
+            <Text style={styles.text5}>LMS tasks deadline in 4days</Text>
+          </View>
+        </View>
+        <Text style={styles.text1}>Older</Text>
+        <ScrollView>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -80,7 +107,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#ffff",
-    width: 170,
+    width: 165,
     height: 60,
     borderRadius: 10,
   },
@@ -89,6 +116,57 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     marginTop: 10,
     marginBottom: 30,
+  },
+  text2: {
+    fontSize: 14,
+    fontWeight: "300",
+    marginLeft: 15,
+  },
+  view5: {
+    // marginLeft: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 25,
+  },
+  view6: {
+    height: 100,
+    width: 185,
+    backgroundColor: "#AD9DFF",
+    borderRadius: 10,
+    marginLeft: 20,
+    padding: 10,
+  },
+  view7: {
+    height: 100,
+    width: 185,
+    backgroundColor: "#AD9DFF",
+    borderRadius: 10,
+    // marginLeft: 20,
+    marginRight: 20,
+    padding: 10,
+  },
+  text3: {
+    color: "#fff",
+    fontSize: 12,
+    fontWeight: "600",
+  },
+  text4: {
+    fontSize: 8,
+    fontWeight: "300",
+    marginLeft: 20,
+    color: "#ffff",
+    opacity: 0.5,
+  },
+  view8: {
+    marginBottom: 2,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text5: {
+    // marginTop: 7,
+    fontSize: 12,
+    fontWeight: "300",
+    color: "#ffff",
   },
 });
 
